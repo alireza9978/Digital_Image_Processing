3d5e94edaca9b1818a1157ce2465f832b306b773
Alireza Rahimi <rahimialireza.az@gmail.com>
2021-01-08 21:11:44 +0330

    change function name

--- six/quantize_wavelet_pyramid.py
+++ six/quantize_wavelet_pyramid.py
@@ -3,7 +3,7 @@ import numpy as np
 import pywt.data
 
 
-def normalize_and_quantize(input_image):
+def quantize(input_image):
     return np.floor(input_image / 2) * 2
 
 
@@ -12,10 +12,10 @@ image = cv.imread("../data/Lena.bmp", cv.IMREAD_GRAYSCALE)
 
 # Wavelet transform of image, and plot approximation and details
 LL, (LH, HL, HH) = pywt.dwt2(image, 'haar')
-LL = normalize_and_quantize(LL)
-LH = normalize_and_quantize(LH)
-HL = normalize_and_quantize(HL)
-HH = normalize_and_quantize(HH)
+LL = quantize(LL)
+LH = quantize(LH)
+HL = quantize(HL)
+HH = quantize(HH)
 reconstruct_image = pywt.idwt2((LL, (LH, HL, HH)), 'haar').astype(np.uint8)
 cv.imwrite("../output/quantized_wavelet.jpg", reconstruct_image)
 cv.imwrite("../output/quantized_wavelet_LL.jpg", LL)
