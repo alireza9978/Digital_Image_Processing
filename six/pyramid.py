81d1e8041b4d817667929b1fb5df68847f2e80a2
Alireza Rahimi <rahimialireza.az@gmail.com>
2021-01-08 21:03:04 +0330

    fix major bug

--- six/pyramid.py
+++ six/pyramid.py
@@ -4,7 +4,7 @@ import numpy as np
 
 # کوچک کردن تصویر با حذف سطر و ستون
 def down_sample_replication(input_image):
-    down_sampled = np.zeros((int(input_image.shape[0] / 2), int(input_image.shape[1] / 2), input_image.shape[2]),
+    down_sampled = np.zeros((int(input_image.shape[0] / 2), int(input_image.shape[1] / 2)),
                             dtype=np.uint8)
     for i in range(down_sampled.shape[0]):
         for j in range(down_sampled.shape[1]):
@@ -12,20 +12,11 @@ def down_sample_replication(input_image):
     return down_sampled
 
 
-# بزرگ کردن تصویر با تکرار مقدار هر پیکسل
-def up_sample_with_replication(input_image):
-    output_image = np.zeros((input_image.shape[0] * 2, input_image.shape[1] * 2, input_image.shape[2]), dtype=np.uint8)
-    for i in range(output_image.shape[0]):
-        for j in range(output_image.shape[1]):
-            output_image[i][j] = input_image[int(np.floor(i / 2))][int(np.floor(j / 2))]
-    return output_image
-
-
 def apply_filter(image, input_filter):
     filter_size = input_filter.shape[0]
     padding_size = (int(filter_size / 2), int(filter_size / 2))
 
-    padded_image = np.pad(image, [padding_size, padding_size, (0, 0)], mode='edge', )
+    padded_image = np.pad(image, [padding_size, padding_size], mode='edge', )
     output_image = np.zeros(image.shape, dtype=np.uint8)
     for i in range(padded_image.shape[0] - input_filter.shape[0]):
         for j in range(padded_image.shape[1] - input_filter.shape[1]):
@@ -39,17 +30,29 @@ def apply_filter(image, input_filter):
     return output_image
 
 
+def subtract(one, two):
+    temp = (one.astype(np.int16) - two.astype(np.int16))
+    temp = temp - temp.min()
+    return ((temp / temp.max()) * 255).astype(np.uint8)
+
+
+def decomposition(main_image, averaged_image):
+    low_pass = subtract(main_image, averaged_image)
+    new_image = subtract(main_image, low_pass)
+    return new_image, low_pass
+
+
 filter_size = 2
 pyramid_level = 3
 
-image = cv.imread("../data/Lena.bmp")
+image = cv.imread("../data/Lena.bmp", cv.IMREAD_GRAYSCALE)
 
 box_filter = np.ones([filter_size, filter_size], dtype=np.uint8) / (filter_size * filter_size)
-new_image = image
+temp_image = image
+cv.imwrite("../output/pyramid_level_{}_main.jpg".format(0), temp_image)
 for i in range(pyramid_level):
-    new_image = apply_filter(new_image, box_filter)
-    new_image = down_sample_replication(new_image)
-    cv.imwrite("../output/pyramid_level_{}.jpg".format(i + 1), new_image)
-for i in range(pyramid_level):
-    new_image = up_sample_with_replication(new_image)
-    cv.imwrite("../output/reconstructed_pyramid_level_{}.jpg".format(pyramid_level - (i + 1)), new_image)
+    filtered_temp_image = apply_filter(temp_image, box_filter)
+    temp_image, high_pass = decomposition(temp_image, filtered_temp_image)
+    cv.imwrite("../output/pyramid_level_{}_main.jpg".format(i + 1), temp_image)
+    cv.imwrite("../output/pyramid_level_{}_high_pass.jpg".format(i + 1), high_pass)
+    temp_image = down_sample_replication(temp_image)
