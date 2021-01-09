9c0fa0ab5e923412bb001fa3a878f9d0644e0650
Alireza Rahimi <rahimialireza.az@gmail.com>
2021-01-09 20:17:09 +0330

    calculate PSNR for three level

--- six/quantize_wavelet_pyramid.py
+++ six/quantize_wavelet_pyramid.py
@@ -10,17 +10,24 @@ def quantize(input_image):
 # Load image
 image = cv.imread("../data/Lena.bmp", cv.IMREAD_GRAYSCALE)
 
+pyramid_level = 3
+
 # Wavelet transform of image, and plot approximation and details
-LL, (LH, HL, HH) = pywt.dwt2(image, 'haar')
-LL = quantize(LL)
-LH = quantize(LH)
-HL = quantize(HL)
-HH = quantize(HH)
-reconstruct_image = pywt.idwt2((LL, (LH, HL, HH)), 'haar').astype(np.uint8)
-cv.imwrite("../output/quantized_wavelet.jpg", reconstruct_image)
-cv.imwrite("../output/quantized_wavelet_LL.jpg", LL)
-cv.imwrite("../output/quantized_wavelet_LH.jpg", LH)
-cv.imwrite("../output/quantized_wavelet_HL.jpg", HL)
-cv.imwrite("../output/quantized_wavelet_HH.jpg", HH)
-PSNR = cv.PSNR(image, reconstruct_image)
-print(PSNR)
+OLD_LL = image
+for i in range(pyramid_level):
+    LL, (LH, HL, HH) = pywt.dwt2(OLD_LL, 'haar')
+    TEMP_LL = LL
+    LL = quantize(LL)
+    LH = quantize(LH)
+    HL = quantize(HL)
+    HH = quantize(HH)
+    reconstruct_image = pywt.idwt2((LL, (LH, HL, HH)), 'haar').astype(np.uint8)
+    cv.imwrite("../output/quantized_wavelet.jpg", reconstruct_image)
+    cv.imwrite("../output/quantized_wavelet_LL.jpg", LL)
+    cv.imwrite("../output/quantized_wavelet_LH.jpg", LH)
+    cv.imwrite("../output/quantized_wavelet_HL.jpg", HL)
+    cv.imwrite("../output/quantized_wavelet_HH.jpg", HH)
+    PSNR = cv.PSNR(OLD_LL, reconstruct_image)
+    print("level_{}_image_size_{}".format(i + 1, OLD_LL.shape[0]), PSNR)
+    TEMP_LL = TEMP_LL - TEMP_LL.min()
+    OLD_LL = ((TEMP_LL / TEMP_LL.max()) * 255).astype(np.uint8)
